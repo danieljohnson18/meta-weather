e9bf11111a4d4879efa289eb99b1fc35821ad965
Daniel Arikawe <47419174+danieljohnson18@users.noreply.github.com>
2021-01-14 05:05:55 +0100

    Made API to meta weather API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, {useState} from 'react';
-import './App.css';
-import Layout from './components/Layout/Layout';
-import LeftAside from './components/LeftAside/LeftAside'
-import Search from './components/LeftAside/Search';
-import RightAside from './components/RightAside/RightAside';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import LeftAside from "./components/LeftAside/LeftAside";
+import Search from "./components/LeftAside/Search";
+import RightAside from "./components/RightAside/RightAside";
+import axios from "axios";
 
 function App() {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState([]);
 
-  return  (
-     <Layout>
-       <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-6 h-screen p-10 bg-blue-100">
-         <aside className="col-span-1 lg:col-span-2 bg-blue-300 h-full rounded-md">
-           <LeftAside toggle={toggle} setToggle={setToggle} />
-           <Search toggle={toggle} setToggle={setToggle}/>
-         </aside>
-         {/*Right Aside*/}
-         <aside className="col-span-1 lg:col-span-4 bg-blue-500 h-full rounded-md">
-           <RightAside/>
-         </aside>
-       </div>
-     </Layout>
+  useEffect(() => {
+    axios
+      .get(
+        "'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/44418/"
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <Layout>
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-6 h-screen p-10 bg-blue-100">
+        <aside className="col-span-1 lg:col-span-2 bg-blue-300 h-full rounded-md">
+          <LeftAside toggle={toggle} setToggle={setToggle} />
+          <Search toggle={toggle} setToggle={setToggle} />
+        </aside>
+        {/*Right Aside*/}
+        <aside className="col-span-1 lg:col-span-4 bg-blue-500 h-full rounded-md">
+          <RightAside />
+        </aside>
+      </div>
+    </Layout>
   );
 }
 
--- src/components/LeftAside/Search.jsx
+++ src/components/LeftAside/Search.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const Search = ({toggle, setToggle}) => {
+const Search = ({ toggle, setToggle }) => {
+  const buttonHandler = () => {
+    setToggle(!toggle);
+  };
 
-    const buttonHandler = () =>{
-        setToggle(!toggle)
-    };
-
-    return (
-        <div className={!toggle ? 'hidden' : ''}>
-            <div className='flex justify-end m-3'>
-                <button onClick={buttonHandler} className='text-white text-xl focu:outline-none'>
-                    &#10005;
-                </button>
-            </div> 
-            <div className="flex items-baseline justify-center">
-                <input 
-                    type="text" 
-                    name="places" 
-                    id="places" 
-                    placeholder="Search for Places"  
-                    className="mt-10 ml-2 p-3 w-auto focus:outline-none focus:ring focus:border-red-100"
-                />
-                <button className="bg-blue-200 p-2 mx-2 rounded-md focus:outline-none 
+  return (
+    <div className={!toggle ? "hidden" : ""}>
+      <div className="flex justify-end m-3">
+        <button
+          onClick={buttonHandler}
+          className="text-white text-xl focu:outline-none"
+        >
+          &#10005;
+        </button>
+      </div>
+      <div className="flex items-baseline justify-center">
+        <input
+          type="text"
+          name="places"
+          id="places"
+          placeholder="Search for Places"
+          className="mt-10 ml-2 p-3 w-auto focus:outline-none focus:ring focus:border-red-100"
+        />
+        <button
+          className="bg-blue-200 p-2 mx-2 rounded-md focus:outline-none 
                     focus:ring-2 focus:ring-purple-200 focus:ring-opacity-50 hover:bg-gray-600 hover:text-white 
                     transition duration-300 ease-in-out text-black"
-                >
-                        Search
-            </button>
-            </div>
-        </div>
-    )
-}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Search;
- 
